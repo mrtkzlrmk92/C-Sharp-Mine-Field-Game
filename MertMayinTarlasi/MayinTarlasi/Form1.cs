b536d21174ccb30c1ba1447f3c2f4ea0485afebb
Mert KIZILIRMAK <114242955+mrtkzlrmk92@users.noreply.github.com>
2023-01-24 12:42:54 +0300

    C-Sharp-Mine-Field-Game

---
+++ MertMayinTarlasi/MayinTarlasi/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            butonuret();
+        }
+
+        private void butonuret()
+        {
+            flowLayoutPanel1.Controls.Clear();
+
+            Random rnd = new Random();
+
+            List<int> mayinlar = new List<int>();
+            for (int i = 1; mayinlar.Count < 15; i++)
+            {
+                int sayi = rnd.Next(1, 69);
+                if (!mayinlar.Contains(sayi))
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+
+
+            for (int i = 1; i < 69; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new System.Drawing.Size(35, 35);
+                btn.Text = i.ToString();
+
+                if (mayinlar.Contains(i))
+                {
+                    btn.Tag = true;
+                }
+                else
+                {
+                    btn.Tag = false;
+                }
+                btn.Click += Btn_Click;
+
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+        int secilen = 0;
+        int bulunan = 0;
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button basilanButton = (Button)sender;
+            bool mayin = (bool)basilanButton.Tag;
+            secilen++;
+            if(mayin)
+            {
+                basilanButton.BackColor = Color.Red;
+                basilanButton.Enabled = false;
+                MessageBox.Show("Mayın bulundu", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bulunan++;
+                secilen--;
+            }
+            else
+            {
+                basilanButton.BackColor = Color.Yellow;
+            }
+            textBox1.Text = secilen.ToString();
+            textBox2.Text = bulunan.ToString();
+
+            if (bulunan>=15)
+            {
+                DialogResult dialog = MessageBox.Show("Mayınlar patladı.Devam etmek ister misiniz?", "Bilgilendirme", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (DialogResult == DialogResult.OK)
+                    {
+                    butonuret();
+                    bulunan = 0;
+                    secilen = 0;
+                    }
+            }
+        }
+    }
+}
---
+++ MertMayinTarlasi/MertMayinTarlasi/Bolge.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MertMayinTarlasi
+{
+    class Bolge : Button, IComparable
+    {
+        private int koordinatX;
+        private int koordinatY;
+
+        public int KoordinatX { get { return koordinatX; } }
+        public int KoordinatY { get { return koordinatY; } }
+
+        private bool mayinliMi;
+        public bool MayinliMi {  get {  return mayinliMi;  } }
+
+        private bool bayrakVarmi;
+        public bool BayrakVarMi { get { return bayrakVarmi; } }
+
+        private bool acildiMi;
+        public bool AcildiMi {  get { return acildiMi;} }
+
+
+        public Bolge(Point p, int koorX, int koorY)
+        {
+            this.Width = 37;
+            this.Height = 37;
+            this.Location = p;
+            koordinatX = koorX;
+            koordinatY = koorY;
+
+            bayrakVarmi = false;
+        }
+
+        public void bayrakDurumunuDegistir()
+        {
+            bayrakVarmi = !bayrakVarmi;
+
+            if (bayrakVarmi)
+                this.Image = Properties.Resources.Bayrak;
+            else
+                this.Image = null;
+        }
+        public void mayinYerlestir()
+        {
+            mayinliMi = true;
+        }
+
+        public void bolgeyiAc (int cevresindekiMayinSayisi)
+        {
+            if (cevresindekiMayinSayisi != 0)
+            {
+                this.Text = cevresindekiMayinSayisi.ToString();
+                this.Font = new Font(FontFamily.GenericSerif, 20, FontStyle.Bold);
+            }
+            this.Enabled = false;
+            this.acildiMi = true;
+
+        }
+
+
+        public void bolgeyiAc()
+        {
+            if (mayinliMi)
+            { 
+                this.Image = Properties.Resources.bombaBayrakli;
+            else
+                this.Image = Properties.Resources.bombaPatlamayan;
+        }
+            else
+        {
+                if (bayrakVarmi)
+                    this.Image = Properties.Resources.yanlisTahmin;
+                else
+                    this.Enabled = false;
+        }
+            this.acildiMi = true;
+        }
+
+
+        public void bolgetyiPatlat()
+        {
+            this.Image = Properties.Resources.bombaTiklanan;
+            acildiMi = true;
+        }
+
+        public int Compareto(object obj)
+        {
+            if (this.koordinatX == (obj as Bolge).koordinatX && this.koordinatY == (obj as Bolge).koordinatY)
+                return 0;
+            else
+                return -1;
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
